d9f48d91120a554c2aac20ee3ecf6d25c8f705a1
DjSelorm <djonathanselorm@gmail.com>
2022-03-10 20:10:26 -0800

    Added files for taxes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	long int y;
+	long long int z;
+	char u;
+	float v;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
+	return (0);i
+	
+}	
