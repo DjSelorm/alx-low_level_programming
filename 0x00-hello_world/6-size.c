bc4a5c13d72d21f0eba12258953f8ac9ebbe024e
DjSelorm <djonathanselorm@gmail.com>
2022-03-13 16:42:55 -0700

    Made corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -8,17 +7,18 @@
  */
 int main(void)
 {
-	int x;
-	long int y;
-	long long int z;
-	char u;
-	float v;
+        int x;
+        long int y;
+        long long int z;
+        char u;
+        float v;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(z));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
-	return (0);
-	
-}	
+        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+        printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(z));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
+        return (0);
+
+}
+
