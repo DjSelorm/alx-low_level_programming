2d49630b71633bd9488b2c8e7f420e628299e879
DjSelorm <djonathanselorm@gmail.com>
2022-03-13 15:45:30 -0700

    made crrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(z);
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
-	return (0);i
+	return (0);
 	
 }	
