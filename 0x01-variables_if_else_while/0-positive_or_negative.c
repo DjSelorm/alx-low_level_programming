a876fa1fea7199736b14e79be309185992b367e8
DjSelorm <djonathanselorm@gmail.com>
2022-03-14 13:50:40 -0700

    made crrectns file for programming cde

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,23 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- *main - prints random values
+ * main - print whether a random number is positive, negative or zero
  *
- *return: 0 on success
+ * Return: 0 on success
  **/
-
 int main(void)
 {
-	/*creates a random number;n*/
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-
+		printf("%i is positive\n", n);
+	if (n < 0)
+		printf("%i is negative\n", n);
+	if (n == 0)
+		printf("0 is zero\n");
 	return (0);
 }
