c813f1f41c08ff231338155a72a87ca1be82834f
DjSelorm <djonathanselorm@gmail.com>
2022-03-21 21:02:42 -0700

    Added files for task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void swap_int(int *a, int *b) - Write a function that swaps
+ * the values of two integers
+ *
+ * @a,@b: numbers.
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a = 98
+	*b = 42
+}
